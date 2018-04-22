dbeab3aa83d6bf1a4c8438be150f3e74732aa9e7
Williams Cordova <willbipo@gmail.com>
2018-04-22 17:10:20 -0500

    Tipos de datos primitivos

---
+++ src/com/williams/exaples/DatosPrimitivos.java
@@ -0,0 +1,12 @@
+package com.williams.exaples;
+
+public class DatosPrimitivos {
+	
+	byte corto = -128;  //de -128 hasta 127
+	int entero = 2147483647; //contiene de -2147483648 a 2147483647
+	long largo = 2147483647; //de  -9223372036854775808 a 9223372036854775807 sin comprobar
+	
+	float flotante = 1.4023984645f;// 1.40239846e–45f a 3.40282347e+38f
+	double doble = 4.94065645841246544324;//4.94065645841246544e–324d  a 1.7976931348623157e+308d
+
+}
